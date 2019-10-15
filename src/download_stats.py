408365fc92380156e04a07b3249264fbecd644fb
Rémi Dupré <r.dupre@qwant.com>
2019-10-15 16:19:53 +0200

    download_stats: parallelize download

--- src/download_stats.py
+++ src/download_stats.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
+import multiprocessing
 import os
-import sys
 import urllib.request
 
 import config
 
 
+def apply_download(dump_file):
+    path = os.path.join(config.STATS_DUMP_DIR, dump_file)
+    url = os.path.join(config.STATS_ENDPOINT, dump_file)
+
+    if os.path.exists(path):
+        return 'skipped existing {}'.format(path)
+
+    # Download the file
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    try:
+        urllib.request.urlretrieve(url, path)
+    except Exception as e:
+        return 'skipped {}: {}'.format(path, e)
+
+    return '{} done'.format(path)
+
+
 def download_stats():
-    for i, dump_file in enumerate(config.STATS_DUMP_FILES):
-        path = os.path.join(config.STATS_DUMP_DIR, dump_file)
+    pool = multiprocessing.Pool(config.WIKIDATA_PARALLEL_DOWNLOADS)
 
-        if os.path.exists(path):
-            print(
-                '({}/{}) Skipped existing {}'.format(
-                    i + 1, len(config.STATS_DUMP_FILES), path
-                )
-            )
-            continue
+    for i, msg in enumerate(
+        pool.imap_unordered(apply_download, config.STATS_DUMP_FILES)
+    ):
+        print('({}/{})'.format(i + 1, len(config.STATS_DUMP_FILES)), msg)
 
-        url = os.path.join(config.STATS_ENDPOINT, dump_file)
-        print('({}/{}) {}'.format(i + 1, len(config.STATS_DUMP_FILES), path))
-
-        # Download the file
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-
-        try:
-            urllib.request.urlretrieve(url, path)
-        except Exception as e:
-            print('Skipped:', e, file=sys.stdout)
+    pool.close()
+    pool.join()
